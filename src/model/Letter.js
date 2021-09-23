ccd43f354be05dfae891f6578c359b59fcbce595
test@leoni.land <nwsf8f3z>
2021-09-23 23:30:58 +0300

    make region and country code - number

--- src/model/Letter.js
+++ src/model/Letter.js
@@ -2,8 +2,8 @@ export class Letter {
     constructor(data) {
         this.name = data.items[0].selectedOptions[0].value;
         this.postal_code = data.shippingPerson.postalCode;
-        this.country_code = data.shippingPerson.countryCode;
-        this.region_code = data.shippingPerson.stateOrProvinceCode;
+        this.country_code = +data.shippingPerson.countryCode;
+        this.region_code = +data.shippingPerson.stateOrProvinceCode;
         this.city = data.shippingPerson.city;
         this.street = data.shippingPerson.street;
         this.order_id = data.id;
