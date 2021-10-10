7c18961467274f43fd7752101632e584a053d6e5
test@leoni.land <nwsf8f3z>
2021-10-10 12:55:22 +0300

    add all letters

--- src/services/index.js
+++ src/services/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export function getOrders(createdFrom, createdTo) {
-    const url = `https://app.ecwid.com/api/v3/1569218/orders?token=secret_Wk9esm1V9SNWQLdXnyhjaVwxQgX68b4L&createdFrom=${createdFrom}&createdTo=${+createdTo+86399}`
+export function getOrders(createdFrom, createdTo, offset = 0) {
+    const url = `https://app.ecwid.com/api/v3/1569218/orders?token=secret_Wk9esm1V9SNWQLdXnyhjaVwxQgX68b4L&createdFrom=${createdFrom}&createdTo=${+createdTo+86399}&offset=${offset}`
     return axios.get(url);
 }
